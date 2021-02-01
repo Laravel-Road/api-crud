3b41e9b7f336e34757cce12b24c20238ef55dce2
Pedro Augusto <pams.pedro@gmail.com>
2021-02-01 13:57:32 -0300

    cria listagem de tarefas

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * @test
+     */
+    public function taskIndex()
+    {
+        // Arrange
+        Task::factory($total = $this->faker->numberBetween(15, 50))->create();
+
+        // Act
+        $response = $this->json('GET', route('tasks.index'));
+
+        // Assert
+        $response
+            ->assertOk()
+            ->assertJsonPath('total', $total);
+    }
+
+    /**
+     * @test
+     */
+    public function taskStore()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    /**
+     * @test
+     */
+    public function taskUpdate()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    /**
+     * @test
+     */
+    public function taskDestroy()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
